c8ee4ec74ba133440673abd40bb14a074a0d71dd
vladstulov <78933990+vladstulov@users.noreply.github.com>
2023-02-21 22:14:51 +0300

    Update views.py

--- prj/sign/views.py
+++ prj/sign/views.py
@@ -6,8 +6,6 @@ from django.shortcuts import redirect
 from django.contrib.auth.models import Group
 from django.contrib.auth.decorators import login_required
 
-#from django.http import HttpResponse
-#API для объектов HttpRequest и HttpResponse, которые определены в модуле django.http.
 
 
 class BaseRegisterView(CreateView):
@@ -22,21 +20,4 @@ def upgrade_me(request):
     if not request.user.groups.filter(name='authors').exists():
         premium_group.user_set.add(user)
     return redirect('/')
-
-# def leo(request):
-#     #HTTPResponse- Каждое представление отвечает за возврат объекта HttpResponse.
-#     return HttpResponse("Знак зодиака Левв!")
-#
-# def scoppio(request):
-#     return HttpResponse("Знак зодиака Скорпион!")
-
-# def zzzz(request, zzz):
-#     if zzz == 'фыва':
-#         return HttpResponse("Щляпа!")
-#     elif zzz == '2':
-#         return HttpResponse("Щляпа2!")
-#     elif zzz == '3':
-#         return HttpResponse("Щляпа3!")
-    #else:
-        #return HttpResponse("Щляпа1!")
 
